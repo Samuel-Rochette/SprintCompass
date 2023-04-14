20ba487e44926e360fdcf3dfe1affac3b291558e
jwgarbett <90791584+jwgarbett@users.noreply.github.com>
2023-04-14 14:47:17 -0400

    Update SprintReportPage.js

--- reactapp/src/pages/SprintReportPage.js
+++ reactapp/src/pages/SprintReportPage.js
@@ -2,6 +2,22 @@ import { useEffect, useReducer, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { graphqlPost } from "../util/index.js";
 import styles from "../styles.js";
+import {
+	Autocomplete,
+	Box,
+	Button,
+	Card,
+	Modal,
+	Paper,
+	Table,
+	TableCell,
+	TableContainer,
+	TableBody,
+	TableRow,
+	TableHead,
+	TextField,
+	Typography,
+} from "@mui/material";
 
 const SprintReportPage = () => {
 	const { sprintId } = useParams();
@@ -10,6 +26,7 @@ const SprintReportPage = () => {
 	const pageLoaded = useRef(false);
 	const [state, setState] = useReducer(reducer, {
 		report: {},
+		stories: [],
 	});
 
 	useEffect(() => {
@@ -45,14 +62,72 @@ const SprintReportPage = () => {
 					variables: { sprintid: sprintId },
 				}
 			);
-			setState({ report: data.sprintreport });
+			setState({ report: data.sprintreport, stories: data.sprintreport.stories });
 			console.log(data.sprintreport);
 		})();
 
 		pageLoaded.current = true;
 	}, []);
 
-	return <div>Sprint Report Page</div>;
+	const returnHome = () => {
+		navigate("/")
+	}
+
+	return (
+		<div>
+			<Card style={{display: 'flex'}}>
+				<h1 style={{marginLeft: "2%"}}>{state.report.name} Report</h1>
+				<Button 
+					variant="contained"
+					style={{marginTop: "1%", marginLeft: "80%", height: "5%", width: "8%"}}
+					onClick={returnHome}
+				>
+					Return Home
+				</Button>
+			</Card>
+				<Card>
+					{state.stories.length > 0 && (
+						<div style={{marginLeft: "5%"}}>
+							{state.stories.map(story => {
+								return (
+									<div key={state.index++}>
+										<Typography style={{textDecorationLine: "underline", fontWeight: "bold", fontSize: 20}}>
+										{story.name}
+										</Typography>
+										<Typography style={{marginLeft: "2%"}}>
+											<b>Status:</b> {story.status}
+										</Typography>
+										<Typography style={{marginLeft: "2%"}}>
+											<b>Description:</b> {story.description}
+										</Typography>
+										<Typography style={{marginLeft: "2%"}}>
+											<b>Hours Estimated:</b> {story.hoursestimated}
+										</Typography>
+										<Typography style={{marginLeft: "2%"}}>
+											<b>Hours Logged:</b> {story.hourslogged}
+										</Typography>
+										{story.tasks.map(task => {
+											return (
+												<div key={state.storyKey++} style={{marginLeft: "10%"}}>
+													<br/>
+													<Typography style={{fontWeight: "bold"}}>
+														{task.name}
+													</Typography>
+													<Typography style={{marginLeft: "5%"}}>
+														Status: {task.status}
+													</Typography>
+												</div>
+											)
+										})}
+										<br/>
+									</div>
+								)
+							})}
+						</div>
+					)}
+				</Card>
+		</div>
+	)
 };
 
 export default SprintReportPage;
